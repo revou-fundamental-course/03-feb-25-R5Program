06abbb50dde5d8aa9aab68f9d32e337dc61faaf6
Ridho Firmansyah <ridhofirmansyah190104@gmail.com>
2025-02-09 16:45:27 +0800

    tambah elemen untuk penjelasan

--- js/app.js
+++ js/app.js
@@ -1,6 +1,15 @@
 const displayInput = document.getElementById("display");
+const displayHasil = document.getElementById("hasil");
+const displayCara = document.getElementById("cara");
 
 function konversi() {
-	const hasil = displayInput.value;
-	console.log(hasil);
+	const hasil = displayInput.value * (9 / 5) + 32;
+	displayHasil.value = `${hasil}\u00B0F`;
+	displayCara.value = `${displayInput.value}\u00B0C * (9/5) + 32 = ${hasil}\u00B0F`;
+}
+
+function reset() {
+	displayInput.value = "";
+	displayHasil.value = "";
+	displayCara.value = "";
 }
