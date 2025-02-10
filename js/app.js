0515b1ee0b628cb972269d234c73be65528b4b16
Ridho Firmansyah <ridhofirmansyah190104@gmail.com>
2025-02-10 22:35:53 +0800

    Code Javascript Succes

--- js/app.js
+++ js/app.js
@@ -1,15 +1,46 @@
 const displayInput = document.getElementById("display");
 const displayHasil = document.getElementById("hasil");
 const displayCara = document.getElementById("cara");
+const labelInput = document.getElementById("input");
+const labelHasil = document.getElementById("output");
+
+let celciusToFahrenheit = true;
 
 function konversi() {
-	const hasil = displayInput.value * (9 / 5) + 32;
-	displayHasil.value = `${hasil}\u00B0F`;
-	displayCara.value = `${displayInput.value}\u00B0C * (9/5) + 32 = ${hasil}\u00B0F`;
+	const inputValue = parseFloat(displayInput.value);
+	if (isNaN(inputValue)) {
+		displayHasil.value = "Masukkan angka!";
+		displayCara.value = "";
+		return;
+	}
+
+	let hasil, cara;
+	if (celciusToFahrenheit) {
+		hasil = `${(inputValue * 9) / 5 + 32}\u00B0F`;
+		cara = `${inputValue}\u00B0C * (9/5) + 32 = ${hasil}`;
+	} else {
+		hasil = `${(((inputValue - 32) * 5) / 9).toFixed(2)}\u00B0C`;
+		cara = `(${inputValue}\u00B0F - 32) * 5/9 = ${hasil}`;
+	}
+	displayHasil.value = hasil;
+	displayCara.value = cara;
 }
 
 function reset() {
 	displayInput.value = "";
 	displayHasil.value = "";
 	displayCara.value = "";
+}
+
+function reverse() {
+	celciusToFahrenheit = !celciusToFahrenheit;
+
+	if (celciusToFahrenheit) {
+		labelInput.innerHTML = "Celcius (&deg;C)";
+		labelHasil.innerHTML = "Fahrenheit (&deg;F)";
+	} else {
+		labelInput.innerHTML = "Fahrenheit (&deg;F)";
+		labelHasil.innerHTML = "Celcius (&deg;C)";
+	}
+	reset();
 }
